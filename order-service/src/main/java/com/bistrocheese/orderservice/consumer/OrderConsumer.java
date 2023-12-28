9de4b32230337386222c01ebe2e14e96bf6546c8
nvtuanqt212uit <winqti@gmail.com>
2023-12-28 15:19:42 +0700

    Dockerize service

--- order-service/src/main/java/com/bistrocheese/orderservice/consumer/OrderConsumer.java
+++ order-service/src/main/java/com/bistrocheese/orderservice/consumer/OrderConsumer.java
@@ -5,6 +5,7 @@ import com.bistrocheese.orderservice.exception.CustomException;
 import com.bistrocheese.orderservice.model.Order;
 import com.bistrocheese.orderservice.model.OrderStatus;
 import com.bistrocheese.orderservice.repository.OrderRepository;
+import com.bistrocheese.orderservice.service.RedisHashService;
 import com.bistrocheese.orderservice.service.impl.OrderServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
@@ -15,12 +16,14 @@ import org.springframework.stereotype.Component;
 import java.util.UUID;
 
 import static com.bistrocheese.orderservice.constant.RabbitConstant.ROUTING_QUEUE;
+import static com.bistrocheese.orderservice.constant.RedisConstant.ORDERS;
 
 @Component
 @RequiredArgsConstructor
 public class OrderConsumer {
 
     private final OrderRepository orderRepository;
+    private  final RedisHashService redisHashService;
     private final Logger logger = LoggerFactory.getLogger(OrderServiceImpl.class);
 
     @RabbitListener(queues = {ROUTING_QUEUE})
@@ -33,7 +36,9 @@ public class OrderConsumer {
 
         logger.info("At orderServiceIml, order completed: {}", order);
 
+
         order.setStatus(OrderStatus.COMPLETED);
+        redisHashService.hSet(ORDERS, message, order);
         orderRepository.save(order);
     }
 }
